3a1a5ebec7f3fc8bd42be006ebb81cf875328627
Tobias Lückel <lueckel@solutiondrive.de>
2019-03-07 17:09:19 +0100

    Adds a first draft of a command which can list snippets in a path

---
+++ Commands/SnippetsList.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright solutionDrive GmbH
+ */
+
+namespace sdDeploymentHelperShopware\Commands;
+
+use sdDeploymentHelperShopware\Services\SnippetsReaderInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SnippetsList extends Command
+{
+    /** @var SnippetsReaderInterface */
+    private $snippetsReader;
+
+    public function __construct(
+        SnippetsReaderInterface $snippetsReader
+    ) {
+        parent::__construct();
+        $this->snippetsReader = $snippetsReader;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('sd:snippets:list')
+            ->setDescription('Lists all snippets that are found in the directory.')
+            ->addOption(
+                'source',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The folder from where the snippets should be imported, relative to Shopware\'s root folder',
+                'snippets'
+            );
+    }
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $sourceDir = $this->container->getParameter('kernel.root_dir') . '/' . $input->getOption('source') . '/';
+
+        $snippets = $this->snippetsReader->readSnippets($sourceDir);
+        foreach ($snippets as $namespace => $data) {
+            $output->writeln($namespace);
+        }
+    }
+
+
+}
